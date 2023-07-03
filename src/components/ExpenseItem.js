5193ee1f05e9a7cf7ce669571912dbd86a32cd19
vijay.bansal <vijay.bansal@mediamonks.com>
2023-07-03 14:22:17 +0530

    chore(dynamic data binding) : dynamic but still static data

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,15 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem(){
+    const expenseDate = new Date(2023, 6, 3);
+    const expenseTitle = "Car Insurance";
+    const expenseAmount = "5000";
     return (
         <div className="expense-item">
-            <div >June 30th 2023</div>
+            <div >{expenseDate.toISOString()}</div>
             <div className="expense-item__description">
-                <h2>Car insurance</h2>
-                <div className="expense-item__price">INR5000</div>
+                <h2>{expenseTitle}</h2>
+                <div className="expense-item__price">INR{expenseAmount}</div>
             </div>
         </div>
     );
