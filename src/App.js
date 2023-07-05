2b99c9a0f2cf2158f7fbb637f1baddc72247fa06
vijay.bansal <vijay.bansal@mediamonks.com>
2023-07-05 17:46:35 +0530

    chore(expense-cp) : using multiple components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import ExpenseItem from "./components/ExpenseItem";
-function App() {
+import Expenses from "./components/Expenses";
+function App(props) {
   const expenses = [
 
     {id: "e1", title : "Car Insurance", amount : "1000", date: new Date(2023, 7, 4)},
@@ -10,26 +10,7 @@ function App() {
   return (
     <div>
       <h2>Expense tracker</h2>
-      <ExpenseItem 
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      ></ExpenseItem>
-      <ExpenseItem 
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      ></ExpenseItem>
-      <ExpenseItem 
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      ></ExpenseItem>
-      <ExpenseItem 
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      ></ExpenseItem>
+      <Expenses expenseItem={expenses} />
     </div>
   );
 }
