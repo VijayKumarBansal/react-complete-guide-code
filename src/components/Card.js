2345938aba8eb6a663f746db50d80621f1b543cc
vijay.bansal <vijay.bansal@mediamonks.com>
2023-07-07 16:01:15 +0530

    chore(component composition) : uses props.children-default

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import './Card.css';
 function Card(props){
     const classes = 'card ' + props.className;
 return(
-    <div className={classes}>{props.children}</div>
+    <div className={classes}>{props.children}</div> //props.children is built in prop we cannot edit it.
 );
 
 }
