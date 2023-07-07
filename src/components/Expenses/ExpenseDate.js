5f26137f25ce17a3fa6479bc7703fe97d4de7c74
vijay.bansal <vijay.bansal@mediamonks.com>
2023-07-07 16:15:10 +0530

    update(components folders) : organizes the components

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -1,4 +1,4 @@
-import Card from './Card';
+import Card from '../UI/Card';
 import './ExpenseDate.css';
 function ExpenseDate(props) {
     const month = props.date.toLocaleString('en-US', {month: 'long'});
